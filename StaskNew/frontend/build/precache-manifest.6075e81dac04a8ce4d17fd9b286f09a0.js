775b860e1ec3ad745e0fd2b19dcccc31fa7511f2
00MRX00 <17515560@mail.ru>
2020-05-03 02:26:53 +0300

    frontend added

---
+++ StaskNew/frontend/build/precache-manifest.6075e81dac04a8ce4d17fd9b286f09a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b31f50e9dbbd3ebff58f6135d6c918",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fc5aa8acbfb5f4751e7",
+    "url": "/static/css/main.52f22684.chunk.css"
+  },
+  {
+    "revision": "b5b734056f93cf4f02d3",
+    "url": "/static/js/2.b1327c2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1327c2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc5aa8acbfb5f4751e7",
+    "url": "/static/js/main.b7e53e63.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
