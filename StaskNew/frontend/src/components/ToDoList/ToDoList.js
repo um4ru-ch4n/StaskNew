cd0eef91bc4b4f5b137aed8904f8429c906d3c33
00MRX00 <17515560@mail.ru>
2020-05-23 17:56:08 +0300

    TodoList modified

--- StaskNew/frontend/src/components/ToDoList/ToDoList.js
+++ StaskNew/frontend/src/components/ToDoList/ToDoList.js
@@ -1,40 +1,67 @@
-import React, {Component} from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+
+
 import { connect } from 'react-redux';
 import { fetchTodos, setCurrentTodo } from '../../store/actions/todo';
 
+
 class ToDoList extends Component {
+    state = {
+        checked: []
+    }
 
     UNSAFE_componentWillMount() {
         this.props.fetchTodos(this.props.currentTask.id);
     }
 
-    renderToDos() {
-        return this.props.todos.map((todo) => {
-            return (
-                <li
-                    key={todo.id}
-                >
-                    <NavLink
-                        to={todo.task + "/" + todo.id}
-                        onClick={() => this.props.setCurrentTodo(todo.id)}
-                    >
-                        {todo.text}
-                    </NavLink>
-                </li>
-            );
-        })
-    }
+    handleToggle = value => () => {
+        const { checked } = this.state
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
 
-   render() {
-       return(
-            <div>
-                <ul>
-                    {this.renderToDos()}
-                </ul>
-            </div>
-       )
-   } 
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        this.setState({
+            checked: newChecked
+        })
+    };
+
+    render() {
+        return (
+            <List>
+                {
+                    this.props.todos.map((todo) => {
+                        const labelId = `checkbox-list-label-${todo.id}`;
+
+                        return (
+                            <ListItem key={todo.id} role={undefined} dense button onClick={this.handleToggle(todo.id)}>
+                                <ListItemIcon>
+                                    <Checkbox
+                                        edge="start"
+                                        checked={this.state.checked.indexOf(todo.id) !== -1}
+                                        tabIndex={-1}
+                                        disableRipple
+                                        inputProps={{ 'aria-labelledby': labelId }}
+                                        color={"primary"}
+                                    />
+                                </ListItemIcon>
+                                <ListItemText id={labelId} primary={todo.text} />
+                            </ListItem>
+                        );
+                    })
+                }
+            </List>
+        )
+    }
 }
 
 function mapStateToProps(state) {
